b5785c3a730c21869e416ee958977073b9f7869e
isaac <isaacsms@id.uff.br>
2020-10-05 17:55:40 -0300

    added comment form on posts. functionallity still in progress

--- views/single-post.php
+++ views/single-post.php
@@ -23,30 +23,53 @@ include_once("./partials/public-header.php");
 <section class="container">
     <div class="row">
         <div class="col-sm-8 ">
-                <div class="card my-4">
-                    <img class="image-fluid card-img-top post-header-img" src="<?php echo "../uploads/". $post['image']?>" alt="">
-                    <div class="card-header ">
-                    
+            <div class="card my-4">
+                <img class="image-fluid card-img-top post-header-img" src="<?php echo "../uploads/" . $post['image'] ?>" alt="">
+                <div class="card-header ">
+
                     <form class="form-inline" action="../" method="post">
-                <h3 class="card-title ">
-                    <?php echo $post['title'];?> <button class="btn btn-info btn-small" name="query" value="<?php echo $post['category'];?>"><?php echo $post['category'];?></button>
-                </h3>
-                </form>
-                        <small class="text-muted">Escrito por <?php echo $post['author'].", ".$post['datetime'].".";?>
-                        
+                        <h3 class="card-title ">
+                            <?php echo $post['title']; ?> <button class="btn btn-info btn-small" name="query" value="<?php echo $post['category']; ?>"><?php echo $post['category']; ?></button>
+                        </h3>
+                    </form>
+                    <small class="text-muted">Escrito por <?php echo $post['author'] . ", " . $post['datetime'] . "."; ?>
+
                     </small>
-     
+
                     <span class="badge post-card-badge">20 comentários</span>
+                </div>
+                <div class="card-body">
+                    <p class="card-text"><?php echo $post['content']; ?></p>
+                </div>
+            </div>
+
+            <form action="../routes/addComment.php?id=<?php echo $post['id']; ?>" method="post">
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <h5>Comente sua opinião!</h5>
                     </div>
                     <div class="card-body">
-                    <p class="card-text"><?php echo $post['content'];?></p>
+                        <div class="form-group">
+                            <input type="text" placeholder="Seu nome" name="reader" class="form-control"  required>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" placeholder="Seu email" name="email" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <textarea cols="80" rows="8" name="comment" class="form-control"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">Enviar</button>
                     </div>
                 </div>
+            </form>
         </div>
         <div class="col-sm-4 ">
 
         </div>
     </div>
 </section>
+
+
 <!-- END CONTENT -->
+
 <?php include_once("./partials/footer.php"); ?>
